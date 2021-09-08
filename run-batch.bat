e0066f4b93f1f68f8e11a0e1527d7d12ae0ab524
svbeuningen <11160433+svbeuningen@users.noreply.github.com>
2021-09-08 15:56:48 +0200

    fixed batch

--- run-batch.bat
+++ run-batch.bat
@@ -1,9 +1,6 @@
 call env\Scripts\activate
-PAUSE
 set /p data=<data.conf
-PAUSE
 echo %data%
-PAUSE
 for /d %%i in (%data%) do (
     echo %%i
     call python batchDetection.py "%%i" > "%%i/output.log"
